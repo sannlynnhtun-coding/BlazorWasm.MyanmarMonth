21ea67e93b9f45de46421cc6e711a7cbd2c02935
Myat Phone Thant <133953352+MClox@users.noreply.github.com>
2023-11-25 01:03:43 +0630

    [Add UI]

---
+++ BlazorWasm.MyanmarMonth/Pages/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorWasm.MyanmarMonth.Pages;
+
+public partial class Index
+{
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            //await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./assets/js/main.js");
+            //var targetUrl = "./assets/js/main.js";
+            //await JsRuntime.InvokeVoidAsync("loadJs", targetUrl);
+
+            await Task.Delay(500);
+            await JsRuntime.InvokeVoidAsync("loadForm");
+        }
+    }
+}
