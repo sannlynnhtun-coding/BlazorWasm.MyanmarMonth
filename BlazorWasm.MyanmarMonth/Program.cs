a4205429e151a5c08acfc354a028328c17057431
Myat Phone Thant <133953352+MClox@users.noreply.github.com>
2023-11-26 02:01:29 +0630

    [Complete] Finalize Myanmar Months

--- BlazorWasm.MyanmarMonth/Program.cs
+++ BlazorWasm.MyanmarMonth/Program.cs
@@ -2,6 +2,7 @@ using BlazorWasm.MyanmarMonth;
 using BlazorWasm.MyanmarMonth.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#main");
@@ -9,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMudServices();
 builder.Services.AddScoped<MonthService>();
 
 await builder.Build().RunAsync();
